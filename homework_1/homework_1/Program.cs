8b2cd6b04ea747c40b3e29005421ff8849ceb021
Yuriy Ponomarev <ponomarev-25@mail.ru>
2024-07-13 22:06:47 +0400

    homework_1

---
+++ homework_1/homework_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!!!");
+
+        Console.Write("Hello ");
+        Console.Write("World");
+        Console.Write("!!!");
+
+        Console.ReadLine();
+    }
+}
