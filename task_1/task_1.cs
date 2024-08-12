319883e23dbf8ae4a731d3d05d7722aa69c26e42
comp <comp@DESKTOP-NMTGNKJ>
2024-08-12 23:50:18 +0400

    homework_5 ver 1.0

---
+++ task_1/task_1.cs
@@ -0,0 +1,26 @@
+﻿
+class task_1
+{
+    static void Main()
+    {
+        string inputText = Console.ReadLine() ?? "";
+
+        PrintSeparateWords(SplitText(inputText));
+        Console.ReadKey();
+    }
+
+    static string[] SplitText(string text)
+    {
+        return text.Split(" ");
+    }
+
+    static void PrintSeparateWords(string[] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            Console.WriteLine(array[i]);
+        }
+        
+    }
+
+}
---
+++ task_2/task_2.cs
@@ -0,0 +1,27 @@
+﻿
+class task_2
+{
+    static void Main()
+    {
+        string inputText = Console.ReadLine() ?? "";
+
+        Reverse(inputText);
+        Console.ReadKey();
+    }
+
+    static string[] SplitText(string text)
+    {
+        return text.Split(" ");
+    }
+
+    static void Reverse(string text)
+    {
+        string[] array = SplitText(text);
+        {
+            for (int i = array.GetLength(0) - 1; i >= 0; i--)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
